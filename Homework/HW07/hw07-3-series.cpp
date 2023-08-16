2cc5fe1539ccd042d78b3a23710eecd4f5cbda30
zarlipzz <guitar30255@gmail.com>
2023-08-17 02:25:02 +0700

    workkk

--- Homework/HW07/hw07-3-series.cpp
+++ Homework/HW07/hw07-3-series.cpp
@@ -16,3 +16,29 @@
         Series = 9 + 99 + 999 + 9999 + 99999 + 999999 + 9999999
         Sum = 11111103
 */
+#include <stdio.h>
+
+int main(){
+    
+    int n,i,n1 = 0, sum = 0;
+    printf("Enter number :\n");
+    scanf("%d",&n);
+    printf("Series = ");
+
+    
+    do{
+        i++;
+        n1 = n1 * 10 + 9 ;
+        sum += n1;
+        
+        printf("%d", n1);
+        
+        if(i <= n-1 && i != n){
+            printf(" + ");
+        }
+
+    } while (i < n);
+    printf("\nSum = %d", sum);
+    
+    return 0;
+}
